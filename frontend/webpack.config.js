2623ba2d4d6f8582fcc3a2eeef0d34d7d65125a0
Fabrizio <fabrizio.silvestri@yahoo.it>
2020-08-02 15:16:51 +0200

    Add webpack configs

---
+++ frontend/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HTMLplugin = require('html-webpack-plugin');
+
+
+const rules = [{
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader'
+  }
+}];
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build')
+  },
+  module: {rules},
+  plugins: [
+    new HTMLplugin({
+      template: './public/index.html'
+    }),
+  ],
+};
